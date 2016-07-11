45b2583daa884bb5f62d3e9c04c4f2f887991806
yejianguang <ye.jg@outlook.com>
2016-07-11 09:51:22 +0800

    1 增加dialog

--- app/src/main/java/com/leaf/gitdemo/MainActivity.java
+++ app/src/main/java/com/leaf/gitdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.leaf.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void buttonClick(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("对话框")
+                .setMessage(getString(R.string.app_name))
+                .setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
     }
 }
